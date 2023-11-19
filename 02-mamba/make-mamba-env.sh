51cd8dacbeb5833b6a6e116707ea8a779bf11f03
Alex Salois <asalois.scholar@gmail.com>
2023-11-19 12:06:18 -0700

    simple mamba env

---
+++ 02-mamba/make-mamba-env.sh
@@ -0,0 +1,7 @@
+module load Mamba/4.14.0-0
+mamba init # only needs to be done once
+source ~/.bashrc # tells mamba where envs are
+mambe create -n test-env # create mamba env call test-env
+mamba activate test-env # actiavte env
+mamba install python=3.10 # install python 3.10
+
