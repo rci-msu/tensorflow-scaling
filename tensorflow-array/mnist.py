54a06fe6cdcb4227fd490b38e85f8e0055cf6a9a
Alex Salois <asalois.scholar@gmail.com>
2023-11-26 19:34:53 -0700

    Added how to run array of same thing

---
+++ tensorflow-array/mnist.py
@@ -0,0 +1,144 @@
+# baseline cnn model for mnist
+import os
+import sys
+from numpy import mean
+from numpy import std
+from models import define_model
+import tensorflow as tf
+from matplotlib import pyplot as plt
+from sklearn.model_selection import KFold
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Flatten
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.callbacks import EarlyStopping
+
+
+# load train and test dataset
+def load_dataset():
+    # load dataset
+    (trainX, trainY), (testX, testY) = mnist.load_data()
+    # reshape dataset to have a single channel
+    trainX = trainX.reshape((trainX.shape[0], 28, 28, 1))
+    testX = testX.reshape((testX.shape[0], 28, 28, 1))
+    # one hot encode target values
+    trainY = to_categorical(trainY)
+    testY = to_categorical(testY)
+    return trainX, trainY, testX, testY
+
+
+# scale pixels
+def prep_pixels(train, test):
+    # convert from integers to floats
+    train_norm = train.astype("float32")
+    test_norm = test.astype("float32")
+    # normalize to range 0-1
+    train_norm = train_norm / 255.0
+    test_norm = test_norm / 255.0
+    # return normalized images
+    return train_norm, test_norm
+
+
+# evaluate a model using k-fold cross-validation
+def evaluate_model(dataX, dataY, vb, n_folds, eps):
+    bsz = 32
+    scores, histories = list(), list()
+    # set up early stopping
+    es = EarlyStopping(monitor="val_accuracy", mode="min", verbose=1, patience=2)
+    # show model
+    model = Sequential()
+    model.add(
+        Conv2D(
+            32,
+            (3, 3),
+            activation="relu",
+            kernel_initializer="he_uniform",
+            input_shape=(28, 28, 1),
+        )
+    )
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Flatten())
+    model.add(Dense(128, activation="relu", kernel_initializer="he_uniform"))
+    model.add(Dense(10, activation="softmax"))
+    # compile model
+    opt = SGD(learning_rate=0.01, momentum=0.9)
+    model.compile(optimizer=opt, loss="categorical_crossentropy", metrics=["accuracy"])
+    model.summary()
+    # prepare cross validation
+    kfold = KFold(n_folds, shuffle=True, random_state=1)
+    # enumerate splits
+    for train_ix, test_ix in kfold.split(dataX):
+        # define model
+        model = Sequential()
+        model.add(
+            Conv2D(
+                32,
+                (3, 3),
+                activation="relu",
+                kernel_initializer="he_uniform",
+                input_shape=(28, 28, 1),
+            )
+        )
+        model.add(MaxPooling2D((2, 2)))
+        model.add(Flatten())
+        model.add(Dense(128, activation="relu", kernel_initializer="he_uniform"))
+        model.add(Dense(10, activation="softmax"))
+        # compile model
+        opt = SGD(learning_rate=0.01, momentum=0.9)
+        model.compile(
+            optimizer=opt, loss="categorical_crossentropy", metrics=["accuracy"]
+        )
+        model.summary()
+        # select rows for train and test
+        trainX, trainY, testX, testY = (
+            dataX[train_ix],
+            dataY[train_ix],
+            dataX[test_ix],
+            dataY[test_ix],
+        )
+        # fit model
+        history = model.fit(
+            trainX,
+            trainY,
+            epochs=eps,
+            batch_size=bsz,
+            validation_data=(testX, testY),
+            verbose=vb,
+            callbacks=[es],
+        )
+        # evaluate model
+        _, acc = model.evaluate(testX, testY, verbose=vb)
+        print("> %.3f" % (acc * 100.0))
+        # stores scores
+        scores.append(acc)
+        histories.append(history)
+    return scores, histories
+
+
+# summarize model performance
+def summarize_performance(scores):
+    # print summary
+    print(
+        "Accuracy: mean=%.3f std=%.3f, n=%d"
+        % (mean(scores) * 100, std(scores) * 100, len(scores))
+    )
+
+
+# run the test harness for evaluating a model
+def run_test_harness():
+    # load dataset
+    trainX, trainY, testX, testY = load_dataset()
+    # prepare pixel data
+    trainX, testX = prep_pixels(trainX, testX)
+    # evaluate model
+    scores, histories = evaluate_model(trainX, trainY, 1, 10, 20)
+    # summarize estimated performance
+    summarize_performance(scores)
+
+
+# entry point, run the test harness
+run_test_harness()
