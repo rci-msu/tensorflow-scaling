eddc9de37b380fc7706229c0e3b9004f5c75ae97
Alex Salois <asalois.scholar@gmail.com>
2023-11-26 19:34:14 -0700

    Used BLACK to format the python files

--- tensorflow-mamba/get_tf_gpu.py
+++ tensorflow-mamba/get_tf_gpu.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
-print("TensorFlow version:", tf.__version__) # print tf version
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU'))) # print how many gpus are available
 
+print("TensorFlow version:", tf.__version__)  # print tf version
+print(
+    "Num GPUs Available: ", len(tf.config.list_physical_devices("GPU"))
+)  # print how many gpus are available
